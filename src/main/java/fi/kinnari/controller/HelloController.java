417d36b17da38eb9de513213e33563e190a4bbc3
Kinnari Pekka <a1503024@myy.haaga-helia.fi>
2017-01-19 16:46:35 +0200

    tehtävä 1

--- src/main/java/fi/kinnari/controller/HelloController.java
+++ src/main/java/fi/kinnari/controller/HelloController.java
@@ -3,14 +3,25 @@ package fi.kinnari.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
 public class HelloController {
 	
-	@RequestMapping("*")
-	public String hello(){
-		return "Hello wörld!";
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(name="name")String name, @RequestParam(name="location")String location){
+		return "Welcome to the "+location+", "+name;
+	}
+	
+	@RequestMapping("/index")
+	public String indeksi(){
+		return "Tämä on etusivu";
+	}
+	
+	@RequestMapping("/contact")
+	public String contacti(){
+		return "Otamme teihin yhteyttä mahdollisimman pian";
 	}
 }
