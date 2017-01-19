ed2f6d335991a69324b1af878d4079c5a6bb8ab0
Kinnari Pekka <a1503024@myy.haaga-helia.fi>
2017-01-19 15:58:08 +0200

    Initial project

---
+++ src/main/java/fi/kinnari/controller/HelloController.java
@@ -0,0 +1,16 @@
+package fi.kinnari.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+	
+	@RequestMapping("*")
+	public String hello(){
+		return "Hello wörld!";
+	}
+}
